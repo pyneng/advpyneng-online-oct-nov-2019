d452531cd586349ce754517984d4846ce53a1842
pyneng <pyneng.course@gmail.com>
2019-10-04 19:27:43 +0000

    01_oop_basics tests

--- exercises/01_oop_basics/conftest.py
+++ exercises/01_oop_basics/conftest.py
@@ -1,9 +1,1 @@
 import pytest
-
-
-def test_attr_or_method(obj, attr=None, method=None):
-    if attr:
-        assert getattr(obj, attr, None) != None, "Атрибут не найден"
-    if method:
-        assert getattr(obj, method, None) != None, "Метод не найден"
-
