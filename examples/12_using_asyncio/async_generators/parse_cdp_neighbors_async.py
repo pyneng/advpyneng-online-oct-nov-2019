b4fae073894cb790b7582c6568b9ecfe85cc301f
natenka <nataliya.samoylenko@gmail.com>
2019-11-06 11:48:52 +0000

    Примеры к следующей лекции

---
+++ examples/12_using_asyncio/async_generators/parse_cdp_neighbors_async.py
@@ -0,0 +1,47 @@
+import re
+from pprint import pprint
+import asyncio
+
+import aiofiles
+
+
+async def get_one_neighbor(filename):
+    async with aiofiles.open(filename) as f:
+        line = ''
+        while True:
+            while not 'Device ID' in line:
+                line = await f.readline()
+            neighbor = line
+            async for line in f:
+                if '----------' in line:
+                    break
+                neighbor += line
+            yield neighbor
+            line = await f.readline()
+            if not line:
+                return
+
+
+def parse_neighbor(output):
+    regex = (
+        r'Device ID: (\S+).+?'
+        r' IP address: (?P<ip>\S+).+?'
+        r'Platform: (?P<platform>\S+ \S+), .+?'
+        r', Version (?P<ios>\S+),')
+
+    result = {}
+    match = re.search(regex, output, re.DOTALL)
+    if match:
+        device = match.group(1)
+        result[device] = match.groupdict()
+    return result
+
+
+async def main():
+    cdp_filename = 'sh_cdp_neighbors_detail.txt'
+    async for neighbor in get_one_neighbor(cdp_filename):
+        print(parse_neighbor(neighbor))
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
