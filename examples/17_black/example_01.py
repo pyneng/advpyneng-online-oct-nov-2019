05ca9251ace76e42062a573d14e14649a99ea8a7
pyneng <pyneng.course@gmail.com>
2019-11-24 08:27:12 +0000

    lecture updates

--- examples/17_black/example_01.py
+++ examples/17_black/example_01.py
@@ -16,3 +16,6 @@ url = (
     "https://github.com/rasbt/python_reference/blob/master/rasbt/python_reference/"
     "blob/master/tutorials/sqlite3_howto/code/print_db_info.py"
 )
+
+if 5 > 1 and 'line' in 'sdfjsldfline' or 1 in [1,2,3,4,5,6,6,7,8] and 'testme' in 'linetest' or not [1,2,3]:
+    pass
